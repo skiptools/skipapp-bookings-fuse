8680fd49b88f08e22c1c146b163895d55d393d0c
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-06-11 00:21:41 -0400

    Update to skip-fuse-ui 1.0.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "TravelBookingsFuse", type: .dynamic, targets: ["TravelBookingsFuse"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.5.23"),
-        .package(url: "https://source.skip.tools/skip-fuse-ui.git", "0.15.7"..<"2.0.0")
+        .package(url: "https://source.skip.tools/skip.git", from: "1.6.0"),
+        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "TravelBookingsFuse", dependencies: [
